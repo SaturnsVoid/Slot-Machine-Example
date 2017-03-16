a08639519ad551b54ca7d560d2d07d411c53c9a5
Adam <saturnsvoid@gmail.com>
2017-03-15 21:12:33 -0400

    Upload

---
+++ SlotMichine/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = RandomChar()
+        TextBox2.Text = RandomChar()
+        TextBox3.Text = RandomChar()
+        If TextBox1.Text = TextBox2.Text AndAlso TextBox2.Text = TextBox3.Text Then
+            MsgBox("Winner!")
+        Else
+            '  MsgBox("Loser!")
+        End If
+    End Sub
+    Public Function RandomChar(Optional validchars As String = "¶■¥©§") As String
+        RandomChar = ""
+        Randomize()
+        RandomChar &= Mid(validchars, Int(Rnd() * validchars.Length) + 1, 1)
+    End Function
+End Class
